a05e3711e88404335add893d1d5e54f9ae2e04f6
Brandon Lee Dodds <brandon.lee.dodds@gmail.com>
2017-06-13 21:43:58 +0000

    Added some nice grid work, my snake is going to be going onto the grid.

--- src/SnakeMachineLearning.py
+++ src/SnakeMachineLearning.py
@@ -1,33 +1,60 @@
 # This project was created by subpanda101 on June the 10th, 2017. May the snake begin!
-import torch as tc
+
 import pygame as pg
+
+# Define constants for each individual colour.
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-HEIGHT = 5
-WIDTH = 5
-MARGIN = 3
+HEIGHT = 16
+WIDTH = 16
+MARGIN = 1
+AMOUNT_PER_LINE = 16
 
+# Create the grid.
 
 grid = []
-for row in range(10):
+for row in range(AMOUNT_PER_LINE):
     # Add an empty array that will hold each cell
     # in this row
     grid.append([])
-    for column in range(10):
+    for column in range(AMOUNT_PER_LINE):
         grid[row].append(0)  # Append a cell
 
-grid[1][5] = 1
 pg.init()
-WINDOW_SIZE = [64,64]
+
+WINDOW_SIZE = [272, 272]
+
 screen = pg.display.set_mode(WINDOW_SIZE)
+
 pg.display.set_caption("Snake Game")
-done=False
+
+done = False
+
 clock = pg.time.Clock()
+
 while not done:
+
+    for event in pg.event.get():
+        if event.type == pg.MOUSEBUTTONDOWN:
+            print("Exiting...")
+            done = True
+
     screen.fill(BLACK)
 
+    # Define the amount of white squares per line.
+    for row in range(AMOUNT_PER_LINE):
+        for column in range(AMOUNT_PER_LINE):
+            color = WHITE
+            pg.draw.rect(screen,
+                         color,
+                         [(MARGIN + WIDTH) * column + MARGIN,
+                          (MARGIN + HEIGHT) * row + MARGIN,
+                          WIDTH,
+                          HEIGHT])
+
     clock.tick(60)
+
     pg.display.flip()
 
 pg.quit()
