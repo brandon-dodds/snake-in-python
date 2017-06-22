c25748070797ce3520350512f44f7852dc9622c8
brandon <brandon.lee.dodds@gmail.com>
2017-06-22 07:57:09 +0100

    Added the random item object.

--- SnakeMachineLearning.py
+++ SnakeMachineLearning.py
@@ -1,12 +1,12 @@
-# This project was created by subpanda101 on June the 10th, 2017. May the snake begin!
-
 import pygame as pg
-
+import random
+import time
 # Defining colour constants.
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (42, 201, 42)
+RED = (240,34,54)
 HEIGHT = 16
 WIDTH = 16
 MARGIN = 3
@@ -65,7 +65,18 @@ class Snake(object):
             Snake.y_coordinate = old_y + 1
             print("RIGHT")
 
+# Creating the random item object.
 
+class randomObject(object):
+    x_coordinate = 0
+    y_coordinate = 0
+
+    def __init__(self):
+        x_coordinate = random.randint(1,15)
+        y_coordinate = random.randint(1,15)
+        grid[x_coordinate][y_coordinate] = 2
+
+# Beginning pygame.
 pg.init()
 
 # The size of the window that will be a constant. Edit if you want the window size to be bigger.
@@ -77,7 +88,7 @@ screen = pg.display.set_mode(WINDOW_SIZE)
 pg.display.set_caption("Snake Game")
 
 snake = Snake(1, 1)
-
+randomItem = randomObject()
 done = False
 
 clock = pg.time.Clock()
@@ -88,38 +99,24 @@ while not done:
         # QUITTING THE PROGRAM.
         if event.type == pg.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
+        
+        # SNAKE DIRECTION MOVEMENT
 
         # USER INPUT.
         elif pg.key.get_pressed()[pg.K_q] != 0:
             done = True
 
         elif pg.key.get_pressed()[pg.K_w] or pg.key.get_pressed()[pg.K_UP]:
-            try:
-                snake.move_snake("UP")
-            except IndexError:
-                print("You broke the snake!")
-                done = True
+            snake.move_snake("UP")
 
         elif pg.key.get_pressed()[pg.K_s] or pg.key.get_pressed()[pg.K_DOWN]:
-            try:
-                snake.move_snake("DOWN")
-            except IndexError:
-                print("You broke the snake!")
-                done = True
+            snake.move_snake("DOWN")
 
         elif pg.key.get_pressed()[pg.K_a] or pg.key.get_pressed()[pg.K_LEFT]:
-            try:
-                snake.move_snake("LEFT")
-            except IndexError:
-                print("You broke the snake!")
-                done = True
+            snake.move_snake("LEFT")
 
         elif pg.key.get_pressed()[pg.K_d] or pg.key.get_pressed()[pg.K_RIGHT]:
-            try:
-                snake.move_snake("RIGHT")
-            except IndexError:
-                print("You broke the snake!")
-                done = True
+            snake.move_snake("RIGHT")
 
         # CONTROL STATEMENTS
 
@@ -137,8 +134,10 @@ while not done:
             color = WHITE
             if grid[row][column] == 0:
                 color = WHITE
-            if grid[row][column] == 1:
+            elif grid[row][column] == 1:
                 color = GREEN
+            elif grid[row][column] == 2:
+                color = RED
             pg.draw.rect(screen,
                          color,
                          [(MARGIN + WIDTH) * column + MARGIN,
